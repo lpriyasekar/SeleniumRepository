fccbb9b93091b3803889f24c08c28d5c0be643d6
Lakshmi Priya <lakshmipriya941993@gmail.com>
2025-03-23 06:24:38 +0530

    Add files via upload
    Week1

---
+++ homeassignment/Chrome.java
@@ -0,0 +1,17 @@
+package homeassignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float a=91.0f;
+		String b="Google";
+		boolean isBeta=false;
+		int releaseYear = 2008;
+		char shortcutKey='C';
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+		System.out.println(isBeta);
+	}
+}
